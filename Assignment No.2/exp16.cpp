f3b1a5a63b0c10f8c8fabb30313f6bd957313299
piyushhh30 <piyushyadav3077@gmail.com>
2026-08-13 17:20:17 +0000

    To find sum of 'n' natural numbers.

---
+++ Assignment No.2/exp16.cpp
@@ -0,0 +1,16 @@
+// To find sum of 'n' natural numbers.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, sum;
+
+    cout<< "Enter a postive number :";
+    cin>> a;
+
+    sum = a * (a + 1)/ 2;
+    cout<< sum << "\n";
+
+
+}
